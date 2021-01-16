9ba69df6f44b210cbe444485005676636b752f96
Tejas Mehta <tmthecoder@gmail.com>
2021-01-15 21:42:36 -0500

    Create a single header and delete the platform specific folders

--- Sources/Argon2Swift.h
+++ Sources/Argon2Swift.h
@@ -8,10 +8,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for Argon2Swift_macOS.
-FOUNDATION_EXPORT double Argon2Swift_macOSVersionNumber;
+FOUNDATION_EXPORT double Argon2SwiftVersionNumber;
 
 //! Project version string for Argon2Swift_macOS.
-FOUNDATION_EXPORT const unsigned char Argon2Swift_macOSVersionString[];
+FOUNDATION_EXPORT const unsigned char Argon2SwiftVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Argon2Swift_macOS/PublicHeader.h>
 
