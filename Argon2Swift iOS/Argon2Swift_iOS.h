b4e0bde5bac0c24dd36b941338237c07feabaa88
Tejas Mehta <tmthecoder@gmail.com>
2021-01-15 21:32:34 -0500

    Added the base cross-platform framework

---
+++ Argon2Swift iOS/Argon2Swift_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Argon2Swift_iOS.h
+//  Argon2Swift iOS
+//
+//  Created by Tejas Mehta on 1/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Argon2Swift_iOS.
+FOUNDATION_EXPORT double Argon2Swift_iOSVersionNumber;
+
+//! Project version string for Argon2Swift_iOS.
+FOUNDATION_EXPORT const unsigned char Argon2Swift_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Argon2Swift_iOS/PublicHeader.h>
+
+
---
+++ Sources/Argon2.swift
@@ -0,0 +1,12 @@
+//
+//  Argon2.swift
+//  Argon2Swift
+//
+//  Created by Tejas Mehta on 1/15/21.
+//
+
+import Foundation
+
+public class Argon2 {
+    
+}
